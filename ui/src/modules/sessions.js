ad28cbe3e1b4072e7dba9414863e1af3ca6c04bf
leonardojoao <ldrsjoao@inf.ufpel.edu.br>
2020-06-16 18:04:44 -0300

    ui: resolving problem with parseInt

--- ui/src/modules/sessions.js
+++ ui/src/modules/sessions.js
@@ -19,13 +19,13 @@ export default {
   mutations: {
     setSessions: (state, res) => {
       Vue.set(state, 'sessions', res.data);
-      Vue.set(state, 'numberSessions', parseInt(res.headers['x-total-count']));
+      Vue.set(state, 'numberSessions', parseInt(res.headers['x-total-count'], 10));
     },
     setSession: (state, data) => {
       if (data) {
         Vue.set(state, 'session', data);
       }
-    }
+    },
   },
 
   actions: {
