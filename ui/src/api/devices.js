d37236b83050164bdd8862d00a16a7e0d93f9a91
vagnersaller <vsaller@yahoo.com>
2020-06-17 12:01:14 -0300

    ui: Formating devices to default export in api

--- ui/src/api/devices.js
+++ ui/src/api/devices.js
@@ -1,30 +1,24 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable one-var */
 import http from '../helpers/http';
-// import http from '@/helpers/http';
 
-export const
-  fetchDevices = async (perPage, page, search) => {
-    let query = '';
-    // eslint-disable-next-line no-trailing-spaces
-    if (search === null) { 
-      query = `/devices?per_page=${perPage}&page=${page}`;
-    } else {
-      query = `/devices?per_page=${perPage}&page=${page}&filter=${search}`;
-    }
-    return http().get(query);
-  },
+const fetchDevices = async (perPage, page, search) => {
+  let query = '';
+  if (search === null) {
+    query = `/devices?per_page=${perPage}&page=${page}`;
+  } else {
+    query = `/devices?per_page=${perPage}&page=${page}&filter=${search}`;
+  }
+  return http().get(query);
+};
 
-  removeDevice = async (uid) => {
-    return http().delete(`/devices/${uid}`);
-  },
+const removeDevice = async (uid) => http().delete(`/devices/${uid}`);
 
-  // eslint-disable-next-line arrow-body-style
-  renameDevice = async (data) => {
-    return http().patch(`/devices/${data.uid}`, { name: data.name });
-  },
+const renameDevice = async (data) => http().patch(`/devices/${data.uid}`, { name: data.name });
 
-  // eslint-disable-next-line arrow-body-style
-  getDevice = async (uid) => {
-    return http().get(`/devices/${uid}`);
-  };
+const getDevice = async (uid) => http().get(`/devices/${uid}`);
+
+export default {
+  fetchDevices,
+  removeDevice,
+  renameDevice,
+  getDevice,
+};
